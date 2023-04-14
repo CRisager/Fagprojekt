cc0c1eb86ecb9dbb0fe78940a7aa01f134577f92
CRisager <chelina.risager@gmail.com>
2023-04-14 15:44:56 +0200

    Update HR_data.py

--- HR_data.py
+++ HR_data.py
@@ -7,24 +7,25 @@ import scipy
 import datetime
 import pytz
 
+### ------------------------------------------------------------------------------------ ### 
 ### Working Directory for physical ###
-# Path for Chelina = "C:/Users/cheli/OneDrive/Skrivebord/Fagprojekt/Fagprojekt_data/physical"
-# Path for Andrea = "/Users/andreabolvig/Desktop/4.semester/Project work/Fagprojekt_data/physical"
-path = "/Users/jesperberglund/Downloads/HR_Data/physical"
+path = "C:/Users/cheli/OneDrive/Skrivebord/Fagprojekt/Fagprojekt_data/physical"
+# path = = "/Users/andreabolvig/Desktop/4.semester/Project work/Fagprojekt_data/physical"
+#path = "/Users/jesperberglund/Downloads/HR_Data/physical"
 os.chdir(path)
 
 # Using list comprehension to loop over all files in folder
-csv_files = [f for f in os.listdir(path) if f.endswith('.csv')]
+csv_files_physical = [f for f in os.listdir(path) if f.endswith('.csv')]
 
 # Load all CSV files from physical lecture into list of data frames
 data_frames_physical = []
-for file in csv_files:
+for file in csv_files_physical:
     file_path = os.path.join(path, file)
     df = pd.read_csv(file_path, skiprows=2, sep=";")
     data_frames_physical.append(df)
 
 start_times_physical = []
-for file in csv_files:
+for file in csv_files_physical:
     file_path = os.path.join(path, file)
     start_time = pd.read_csv(file_path, skiprows=1, nrows=0, sep=";").columns[0].split(": ")[1]
     start_time = datetime.datetime.strptime(start_time, "%d.%m.%Y %H:%M:%S")
@@ -32,38 +33,37 @@ for file in csv_files:
 
 
 ### ------------------------------------------------------------------------------------ ###
-
 ### Working Directory for virtual ###
-# Path for Chelina = "C:/Users/cheli/OneDrive/Skrivebord/Fagprojekt/Fagprojekt_data/virtual"
-# Path for Andrea = "/Users/andreabolvig/Desktop/4.semester/Project work/Fagprojekt_data/virtual"
-path = "/Users/jesperberglund/Downloads/HR_Data/virtual"
+
+path = "C:/Users/cheli/OneDrive/Skrivebord/Fagprojekt/Fagprojekt_data/virtual"
+# path = "/Users/andreabolvig/Desktop/4.semester/Project work/Fagprojekt_data/virtual"
+# path = "/Users/jesperberglund/Downloads/HR_Data/virtual"
 os.chdir(path)
 # Using list comprehension to loop over all files in folder
-csv_files = [f for f in os.listdir(path) if f.endswith('.csv')]
+csv_files_virtual = [f for f in os.listdir(path) if f.endswith('.csv')]
 
-# Load all CSV files from virtual lecture into a list of data frames
+# Load all CSV files from physical lecture into list of data frames
 data_frames_virtual = []
-for file in csv_files:
+for file in csv_files_virtual:
     file_path = os.path.join(path, file)
     df = pd.read_csv(file_path, skiprows=2, sep=";")
     data_frames_virtual.append(df)
 
 start_times_virtual = []
-for file in csv_files:
+for file in csv_files_virtual:
     file_path = os.path.join(path, file)
-    st = pd.read_csv(file_path, skiprows=1, nrows=0, sep=";").columns.values
-    st_str = st.item()
-    st_str = st.item().split(': ')[1]
-    st_datetime = pd.to_datetime(st_str, dayfirst=True)
-    start_times_virtual.append(st_datetime)
-for i in range(len(start_times_virtual)):
-    print(start_times_virtual[i])
+    start_time = pd.read_csv(file_path, skiprows=1, nrows=0, sep=";").columns[0].split(": ")[1]
+    start_time = datetime.datetime.strptime(start_time, "%d.%m.%Y %H:%M:%S")
+    start_times_virtual.append(start_time)
+    
+### ------------------------------------------------------------------------------------ ###
+### Update data frame with time stamps and beats per minute
 
-d = data_frames_physical
+d = data_frames_physical 
 starts = start_times_physical
 for i in range(len(d)):
     d0 = d[i]
-    d0 = d0.drop("Unnamed: 3", axis=1)
+    d0 = d0.drop("Unnamed: 3", axis=1) # Drop empty column
     # Ensure the corrected and non-corrected accounts for all data
     RR = np.cumsum(d0["RR"])
     cRR = np.cumsum(d0["Artifact corrected RR"])
@@ -71,17 +71,21 @@ for i in range(len(d)):
         print("The cumulative times are not the same for corrected and uncorrected")
         print(f"For subject {i}, there is a difference of {np.nanmax(RR)-np.nanmax(cRR)}")
     # Drop rows with NaN, due to dissimilar number of "R-R" recordings due to artifacts
-    d0 = d0.dropna()
-    # Compute new cRR without NaN
+    d0 = d0.dropna() # Drop empty rows
+    # Compute new cRR without NaN (empty rows and colums)
     cRR = np.cumsum(d0["Artifact corrected RR"])
     s0 = starts[i]
-    #Time = [pd.to_datetime(s0.to_pydatetime() + datetime.timedelta(seconds=i/1000))[0] for i in cRR]
-    Time = [pd.Timestamp(s0) + pd.Timedelta(seconds=i/1000) for i in cRR]
-    d0["Time"] = Time
+    # Create a list of time stamps for each data point
+    Time = [pd.Timestamp(s0) + pd.Timedelta(seconds=i/1000) for i in cRR] 
+    d0["Time"] = Time # Add the time stamp column to the data frame
     # Calculate Heart Rate pr. min (bpm) from the R-R intervals
     bpm = 60/(d0["Artifact corrected RR"]/1000)
-    d0["Heart Rate"] = bpm
+    d0["Heart Rate"] = bpm # Add column of HR (beats per minute) to the data frame
     d[i] = d0
+    
+### ------------------------------------------------------------------------------------ ###
+### Plots
+
 
 # Plot with the actual times
 fig, ax = plt.subplots(1,1)
@@ -111,6 +115,7 @@ sns.lineplot(data = d[1], x="Time", y="Heart Rate", color = "blue")
 plt.title("BPM based on corrected RR")
 plt.show()
 
+### ------------------------------------------------------------------------------------ ###
 ### Li's code ###
 # They are misaligned
 # Interpolation and resampling at common time points
