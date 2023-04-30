eaa1846df27d9ead1ac1e034d680a24735412134
CRisager <chelina.risager@gmail.com>
2023-04-30 13:06:05 +0200

    Debugging loading
    The order of files changed depending on the computer. This is now fixed

--- HR_load_and_clean.py
+++ HR_load_and_clean.py
@@ -12,21 +12,34 @@ import matplotlib.dates as mdates
 ### ------------------------------------------------------------------------------------ ### 
 ### Load Data ###
 
+# Loading the data files
 # Physical
 path1 = "C:/Users/cheli/OneDrive/Skrivebord/Fagprojekt/Fagprojekt_data/physical"
 #path = "/Users/andreabolvig/Desktop/4.semester/Project work/Fagprojekt_data/physical"
 #path = "/Users/jesperberglund/Downloads/HR_Data/physical"
 os.chdir(path1)
-# Using list comprehension to loop over all files in folder (minus the teacher and Chelina)
-csv_files_physical = [f for f in os.listdir(path1) if f.endswith('.csv')][:len(os.listdir(path1))-2]
+# Using list comprehension to loop over all files in folder minus the teacher
+csv_files_physical = [f for f in os.listdir(path1) if f.endswith('.csv')][:-1]
+# Sort the files from smallest to largest device number
+csv_files_physical = sorted(csv_files_physical, key=lambda x: int(x.split('.')[0]))
+# Read the teacher file independently
+Teacher_phy = os.listdir(path1)[-1]
+# Create a seperate list with the students AND the teacher
+all_files_virtual = csv_files_physical + [Teacher_phy]
 
 # Virtual
 path2 = "C:/Users/cheli/OneDrive/Skrivebord/Fagprojekt/Fagprojekt_data/virtual"
 #path = "/Users/andreabolvig/Desktop/4.semester/Project work/Fagprojekt_data/virtual"
 #path = "/Users/jesperberglund/Downloads/HR_Data/virtual"
 os.chdir(path2)
-# Using list comprehension to loop over all files in folder
-csv_files_virtual = [f for f in os.listdir(path2) if f.endswith('.csv')][:len(os.listdir(path2))-2]
+# Using list comprehension to loop over all files in folder minus the teacher
+csv_files_virtual = [f for f in os.listdir(path2) if f.endswith('.csv')][:-1]
+# Sort the files from smallest to largest device number
+csv_files_virtual = sorted(csv_files_virtual, key=lambda x: int(x.split('.')[0]))
+# Read the teacher file independently
+Teacher_vir = os.listdir(path2)[-1]
+# Create a seperate list with the students AND the teacher
+all_files_virtual = csv_files_virtual + [Teacher_vir]
 
 
 # Create lists for dataframes and starting times
@@ -35,7 +48,7 @@ data_frames_virtual = []
 start_times_physical = []
 start_times_virtual = []
 
-# Function for loading data
+# Function for loading data into dataframes and starting times
 def Load_data(csv_files, data_frames, start_times, path):
     for file in csv_files:
         file_path = os.path.join(path, file)
