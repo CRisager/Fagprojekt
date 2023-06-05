9e37f5724ea00d68eeebe4ad21e6dfc58f004b1e
andreamlb <s211636@dtu.dk>
2023-06-05 13:40:59 +0200

    frequency

--- Frequency_domain.py
+++ Frequency_domain.py
@@ -1,5 +1,4 @@
-from Split_signals import pd, sns, plt, datetime, np, mdates, dphy_resampled, dvir_resampled, dphy_students, dvir_students, phy_sections, vir_sections, df_quiz_phy, df_quiz_vir
-
+from Split_signals import (plt, np, phy_sections, vir_sections, df_quiz_phy, df_quiz_vir)
 # Importing dependencies
 from IPython.display import Image, Audio
 from datetime import datetime
@@ -11,6 +10,7 @@ from scipy.signal import detrend, welch, windows
 from obspy.signal.util import next_pow_2
 import matplotlib.pyplot as plt
 
+print("hej1")
 
 RR = phy_sections[0][0]['RR']
 
@@ -20,6 +20,7 @@ timestamp = np.cumsum(RR)
 #plt.figure(figsize = (15,7))
 #plt.plot(timestamp, RR, '-o')
 #plt.show()
+print("hej2")
 
 # Interpolate
 f = interp1d(timestamp, RR, 'linear')
@@ -27,6 +28,7 @@ f = interp1d(timestamp, RR, 'linear')
 # Sample rate for interpolation
 fs = 10.0
 steps = 1 / fs
+print("hej3")
 
 # Now we can sample from interpolation function
 timeindex_inter = np.arange(np.min(timestamp), np.max(timestamp), steps)
@@ -36,14 +38,22 @@ rr_interpolated = f(timeindex_inter)
 #plt.plot(timestamp, RR)
 #plt.plot(timeindex_inter, rr_interpolated, 'o')
 #plt.show()
+#print("hej4")
 
 # Detrend time-series (to remove slow drifts)
 rr_interpolated = detrend(rr_interpolated)
+print("hej5")
+
 
 # Plotting the power spectrum
 nfft = next_pow_2(len(rr_interpolated))
+print("hej6")
+
 window = windows.hamming(len(rr_interpolated)//4)
+print("hej7")
+
 freqs, PSD = welch(rr_interpolated, fs=fs, window=window, nfft=nfft, scaling='density', return_onesided=True, detrend=False)
+print("hej8")
 
 plt.figure(figsize = (15,7))
 plt.plot(freqs, PSD)
@@ -53,3 +63,5 @@ plt.xlabel('Frequency')
 plt.ylabel('Power spectrum')
 plt.title("FFT Spectrum (Welch's periodogram)")
 plt.show()
+
+print("hej")
