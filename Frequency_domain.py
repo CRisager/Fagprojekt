cb7c9a677a08e69fc3a05c881c383ae89ad6ad1a
andreamlb <s211636@dtu.dk>
2023-06-05 11:56:51 +0200

    ja

--- Frequency_domain.py
+++ Frequency_domain.py
@@ -1,15 +1,4 @@
-from Resampling import (pd, sns, plt, datetime, np, mdates, dphy_resampled, dvir_resampled, Cutting,
-                        phy_lecture_start_time, phy_lecture_end_time, vir_lecture_start_time, 
-                        vir_lecture_end_time, dphy_students, dvir_students, df_quiz_phy, df_quiz_vir)
-
-from Split_signals import (phy_break_start_time, phy_break_end_time, vir_break_start_time, vir_break_end_time, 
-                           phy_length_before_break, phy_length_after_break, vir_length_before_break, vir_length_after_break, 
-                           phys_starttime1, phys_starttime2, phys_starttime3, phys_starttime4, phys_endtime4, phys_starttime5, 
-                           phys_starttime6, phys_starttime7, phys_endtime7, vir_starttime1, vir_starttime2, vir_endtime2, 
-                           vir_starttime3, vir_starttime4, vir_starttime5, vir_starttime6, vir_endtime6)
-
-from Split_signals import (pd, sns, plt, datetime, np, mdates, dphy_resampled, dvir_resampled,
-                           dphy_students, dvir_students, phy_sections, vir_sections, df_quiz_phy, df_quiz_vir)
+from Split_signals import pd, sns, plt, datetime, np, mdates, dphy_resampled, dvir_resampled, dphy_students, dvir_students, phy_sections, vir_sections, df_quiz_phy, df_quiz_vir
 
 # Importing dependencies
 from IPython.display import Image, Audio
@@ -22,13 +11,15 @@ from scipy.signal import detrend, welch, windows
 from obspy.signal.util import next_pow_2
 import matplotlib.pyplot as plt
 
+
 RR = phy_sections[0][0]['RR']
 
 # Creating the time stamp
 timestamp = np.cumsum(RR)
 # Plotting R-R interval prior to interpolation
-plt.figure(figsize = (15,7))
-plt.plot(timestamp, RR, '-o')
+#plt.figure(figsize = (15,7))
+#plt.plot(timestamp, RR, '-o')
+#plt.show()
 
 # Interpolate
 f = interp1d(timestamp, RR, 'linear')
@@ -41,9 +32,10 @@ steps = 1 / fs
 timeindex_inter = np.arange(np.min(timestamp), np.max(timestamp), steps)
 rr_interpolated = f(timeindex_inter)
 
-plt.figure(figsize = (15,7))
-plt.plot(timestamp, RR)
-plt.plot(timeindex_inter, rr_interpolated, 'o')
+#plt.figure(figsize = (15,7))
+#plt.plot(timestamp, RR)
+#plt.plot(timeindex_inter, rr_interpolated, 'o')
+#plt.show()
 
 # Detrend time-series (to remove slow drifts)
 rr_interpolated = detrend(rr_interpolated)
