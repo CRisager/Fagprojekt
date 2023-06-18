5b1cf44c79b50da2f076f99ae6df61fa32a014f2
jesp9435 <jesp9435@gmail.com>
2023-06-18 16:50:23 +0200

    Update LinearModel.py

--- LinearModel.py
+++ LinearModel.py
@@ -14,36 +14,6 @@ for i in range(len(column)):
         column[i] = 0
 
 df['State'] = column
-"""
-fit_data = df[:-1]
-
-test_data = df.iloc[-1]
-
-X_train = fit_data[['HR_device', 'Number_of_friends', 'State', 'Average_BPM',
-       'TeacherStudent_corr', 'Avg_student_corr', 'GC_teacher_to_student',
-       'GC_student_to_teacher']]
-
-y_train = fit_data['Quiz_score']
-
-regr = linear_model.LinearRegression()
-
-X_test = test_data[['HR_device', 'Number_of_friends', 'State', 'Average_BPM',
-       'TeacherStudent_corr', 'Avg_student_corr', 'GC_teacher_to_student',
-       'GC_student_to_teacher']].values.reshape(1, -1)  # Convert to a 2D array
-
-# Convert back to DataFrame with correct column names
-X_test = pandas.DataFrame(X_test, columns=X_train.columns)
-
-y_test = test_data['Quiz_score']
-
-regr.fit(X_train, y_train)
-
-predicted = regr.predict(X_test)
-
-print(predicted)
-
-print("Error:", y_test - predicted)
-"""
 
 X = df[['HR_device', 'Number_of_friends', 'State', 'Average_BPM',
        'TeacherStudent_corr', 'Avg_student_corr', 'GC_teacher_to_student',
